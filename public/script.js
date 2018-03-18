a521cf0c6cd6919fee6ef5099d6ed13727d3cea6
Kristian Gavran <krga@users.noreply.github.com>
2018-03-18 11:56:35 +0100

    adding search functionality

--- public/script.js
+++ public/script.js
@@ -3,7 +3,16 @@ var vm = new Vue({
   data: {
     cars: [],
     info: '',
+    searchname: '',
     apiUrl: 'http://localhost:3000/api/cars'
+  },
+  
+  watch: {
+    // whenever search filed changes, this function will run
+    searchname: function (newInput, oldInput) {
+      this.info = 'typing...';
+      this.getCars();
+    }
   },
   created: function () {
     this.getCars();
@@ -12,7 +21,7 @@ var vm = new Vue({
     getCars: function () {
       this.info = 'Loading...'
       var vm = this
-      axios.get(vm.apiUrl)
+      axios.get(vm.apiUrl+'?name='+this.searchname)
         .then(function (response) {
           vm.cars = response.data.data;
           vm.info = 'Loading finished!'
